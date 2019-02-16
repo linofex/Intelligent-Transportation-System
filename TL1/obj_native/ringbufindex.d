9475ba8e67724c23d747599c761a1e062215cfab
linofex <alessandro.noferi@gmail.com>
2019-02-16 12:48:02 +0100

    removed files

--- TL1/obj_native/ringbufindex.d
+++
@@ -1,8 +0,0 @@
-obj_native/ringbufindex.o: /home/user/contiki/core/lib/ringbufindex.c \
- /home/user/contiki/core/./lib/ringbufindex.h \
- /home/user/contiki/platform/native/./contiki-conf.h \
- /home/user/contiki/platform/native/./ctk/ctk-curses.h project-conf.h
-/home/user/contiki/core/lib/ringbufindex.c :
- /home/user/contiki/core/./lib/ringbufindex.h :
- /home/user/contiki/platform/native/./contiki-conf.h :
- /home/user/contiki/platform/native/./ctk/ctk-curses.h project-conf.h :
